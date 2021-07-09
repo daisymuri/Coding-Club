6da542c4b695c4103b85ec27ddb5aa8765e84c83
Daisy Murillo <daisy21murillo@gmail.com>
2021-07-09 16:51:47 -0700

    init

---
+++ intro_python.py
@@ -0,0 +1,64 @@
+# Python can be used for many different fields
+# Data Science, Machine Learning, Web Development, & more. 
+# Strong Community - Packages for everything 
+# Easy to Learn 
+
+length = 10 
+width = 20 
+area = length * width 
+
+#Python infers the type of variable - what are variables?
+# int - whole numbers 
+# float - decimals 
+# str - text 
+# bool - True or False value
+
+#taxes calculator 
+
+#Strings, Inputs, & Outputs 
+# example: my_name = input("What's your name?")
+
+#age calculator 
+
+age = int(input("How old are you? \n"))
+
+decades = age//10
+years = age % 10
+
+print("You are " + str(decades) + " decades and " + str(years) + " years old.")
+
+
+#Conditionals - How do we make decisions? 
+# A conditional statement, or if statement 
+
+# 6 Comparators 
+# < less than 
+# <= less than equal to 
+# == equal 
+# >= greater than equal to 
+# > greater than 
+# != NOT equal 
+
+# conditional must equal True 
+# examples : 
+# True and True = ? 
+# True and False = ? 
+# False and False = ? 
+# True or False = ? 
+# True or True = ? 
+# False or False = ? 
+
+# rock, paper, scissors game
+# import modules - random, requests 
+# Python Standard Library https://docs.python.org/3/library/  
+# roll dice game 
+
+# List & Loops 
+# What is a List? Container of Things. What kind? 
+# empty, strings, numbers, mixed items, lists 
+
+acronyms = ['LOL', 'IDK', 'SMH', 'TBH']
+acronyms[0]
+
+# A for loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set, or a string).
+# while loop we can execute a set of statements as long as a condition is true.
